15ef4e3564f18119e6c0ab2f49cf63a527e486df
tbdyzm <1320007028@qq.com>
2020-11-17 18:37:54 +0800

    第二次作业

--- 202021090107-第二次实验/code/client2.c
+++ 202021090107-第二次实验/code/client2.c
@@ -25,6 +25,28 @@ int main(int argc, char **argv){
 
     if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
         perror("can't connect to server\n");
+    
+    char sends[1024], recvs[1024];
+    memset(sends, 0, sizeof(sends));
+    memset(recvs, 0, sizeof(recvs));
+    while (1)
+    {
+        printf("Please enter a string that you want to send to the server: ");
+        fgets(sends, sizeof(sends), stdin);
+        if(send(sockfd, sends, strlen(sends)-1, 0) == -1)
+            perror("send error\n");
+        if(strcmp(sends, "quit\n") == 0 || strcmp(sends, "exit\n") == 0){
+            close(sockfd);
+            break;
+        }
+        if(recv(sockfd, recvs, sizeof(recvs), 0) == -1)
+            perror("recv error\n");
+        fputs(recvs, stdout);
+        memset(sends, 0, sizeof(sends));
+        memset(recvs, 0, sizeof(recvs));
+        printf("\n");
+    }
+
     close(sockfd);
     return 0;
 }
--- 202021090107-第二次实验/code/server2.c
+++ 202021090107-第二次实验/code/server2.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
     struct sockaddr_in server_addr, client_addr;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    
+
     int opt = SO_REUSEADDR;
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -21,23 +21,37 @@ int main(int argc, char **argv){
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(9999);
 
-    if(bind(listenfd, (struct sockaddr*)& server_addr,sizeof(server_addr)))
+    if(bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
         perror("bind error\n");
-
+    
     if(listen(listenfd, 10) < 0)
         perror("listen error\n");
 
     char buff[1024];
-
+    memset(buff, 0, sizeof(buff));
     while(1){
         clilen = sizeof(client_addr);
-
-        if(connfd = accept(listenfd, (struct sockaddr*) &client_addr, &clilen) < 0)
+        if((connfd = accept(listenfd, (struct sockaddr*)&client_addr, &clilen)) < 0)
             perror("accept error\n");
         printf("client address: %s\n", inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
+        while(1){
+            if(recv(connfd, buff, sizeof(buff), 0) == -1)
+                perror("recv error\n");
+            if(strcmp(buff, "quit") == 0 || strcmp(buff, "exit") == 0){
+                close(connfd);
+                break;
+            }
+            int len = strlen(buff);
+            for(int i = 0; i < len/2; i++){
+                char tmp = buff[i];
+                buff[i] = buff[len-1 - i];
+                buff[len-1 - i] = tmp;
+            }
+            if(send(connfd, buff, strlen(buff), 0) == -1)
+                perror("send error\n");
+            memset(buff, 0, sizeof(buff));
+        }
         close(connfd);
     }
-
     close(listenfd);
-    return 0;
 }
